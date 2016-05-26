579c7efffd667755432c372d7442200895f219e4
poohjh0515 <poohjh0515@gmail.com>
2016-05-26 14:29:01 -0500

    fixed

--- main_phytoMorph.sh
+++ main_phytoMorph.sh
@@ -4,6 +4,6 @@ wget -O sayhi.sh https://bitbucket.org/leeatuw/repository_experiment/raw/master/
 echo "Download ended"
 echo "shell started"
 chmod +x sayhi.sh
-sayhi.sh
+./sayhi.sh
 echo "shell ended"
 
