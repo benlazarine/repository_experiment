1d3ac12a799cf07b6ca81734fd187546814e2473
poohjh0515 <poohjh0515@gmail.com>
2016-05-26 13:02:50 -0500

    sayhi.sh delted

--- repotest.sh
+++ repotest.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-wget https://bitbucket.org/leeatuw/repository_experiment/raw/dev/readme.txt
+echo "Download started"
+wget https://bitbucket.org/leeatuw/repository_experiment/raw/master/sayhi.sh
+echo "Download ended"
+echo "shell started"
+sayhi.sh
+echo "shell ended"
 
