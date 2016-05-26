6790f1aafbaacdbe8744eaae52b714a0b18f99d5
poohjh0515 <poohjh0515@gmail.com>
2016-05-26 13:21:25 -0500

    file modified

--- sayhi.sh
+++ sayhi.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "Hi"
+curl -H "Pragma:" --retry 30 --retry-delay 6 -o v840.zip http://proxy.chtc.wisc.edu/SQUID/ndmiller/v840.zip
+unzip -q v840.zip
+export MCR_CACHE_ROOT=$PWD/mcr_cache
+mkdir -p $MCR_CACHE_ROOT
+./run_singleSeedlingImage.sh "MATLAB_Compiler_Runtime/v840/"
