dcbf93009a4fa99df4e0961d4450a51e1c19b17f
poohjh0515 <poohjh0515@gmail.com>
2016-06-01 14:56:51 -0500

    update

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/opt/imagePhenomics/phytoG/run_phytoG.sh /opt/imagePhenomics/common/MATLAB/MATLAB_Compiler_Runtime_R2012a/v717
+sudo /opt/imagePhenomics/phytoG/run_phytoG.sh /opt/imagePhenomics/common/MATLAB/MATLAB_Compiler_Runtime_R2012a/MATLAB_Compiler_Runtime/v717
--- sub_phytoMorph.sh
+++ sub_phytoMorph.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ################################################################################################################
+mkdir -p /opt/imagePhenomics/common/
 echo "Start download: MCR-v717.zip"
-wget -O v717.zip http://davos.cyverse.org/irods-rest/rest/fileContents/iplant/home/nmiller/publicData/v717.zip?ticket=fCMgrszgk7H3Dy2
+wget -O /opt/imagePhenomics/common/v717.zip http://davos.cyverse.org/irods-rest/rest/fileContents/iplant/home/nmiller/publicData/v717.zip?ticket=fCMgrszgk7H3Dy2
 echo "Done download: MCR-v717.zip"
 ################################################################################################################
 ################################################################################################################
@@ -10,8 +11,11 @@ wget -O iPlant_ver0 https://bitbucket.org/leeatuw/repository_experiment/raw/mast
 echo "Done download: iPlant_ver0"
 ################################################################################################################
 ################################################################################################################
+echo "Create directory for phytoG"
+mkdir -p /opt/imagePhenomics/phytoG/
 echo "Start download: phytoG"
-wget -O phytoG https://bitbucket.org/leeatuw/repository_experiment/raw/master/phytoG
+wget -O /opt/imagePhenomics/phytoG/phytoG https://bitbucket.org/leeatuw/repository_experiment/raw/master/phytoG
+sudo chmod +x /opt/imagePhenomics/phytoG/phytoG
 echo "Done download: phytoG"
 ################################################################################################################
 ################################################################################################################
@@ -21,13 +25,15 @@ echo "Done download: launch.sh"
 ################################################################################################################
 ################################################################################################################
 echo "Start download: run_phytoG.sh "
-wget -O run_phytoG.sh https://bitbucket.org/leeatuw/repository_experiment/raw/master/run_phytoG.sh 
+wget -O /opt/imagePhenomics/phytoG/run_phytoG.sh https://bitbucket.org/leeatuw/repository_experiment/raw/master/run_phytoG.sh
+sudo chmod +x /opt/imagePhenomics/phytoG/run_phytoG.sh  
 echo "Done download: run_phytoG.sh "
 ################################################################################################################
 ################################################################################################################
 echo "start unzip: vv717.zip"
 mkdir -p /opt/imagePhenomics/common/MATLAB/MATLAB_Compiler_Runtime_R2012a/
-unzip -q v717.zip -C /opt/imagePhenomics/common/MATLAB/MATLAB_Compiler_Runtime_R2012a/
+#unzip /opt/imagePhenomics/common/v717.zip -C /opt/imagePhenomics/common/MATLAB/MATLAB_Compiler_Runtime_R2012a/
+sudo unzip /opt/imagePhenomics/common/v717.zip -d /opt/imagePhenomics/common/MATLAB/MATLAB_Compiler_Runtime_R2012a/
 echo "Done unzip: vv717.zip"
 ################################################################################################################
 ################################################################################################################
@@ -38,5 +44,6 @@ echo "Done export: vv717.zip"
 ################################################################################################################
 echo "Start run: shell for compile"
 mkdir -p $MCR_CACHE_ROOT
-./launch.sh
+sudo chmod +x launch.sh
+sudo ./launch.sh
 echo "Done run: shell for compile"
