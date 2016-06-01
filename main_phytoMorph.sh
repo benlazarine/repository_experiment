756dd980ce76d07705cb2fdf956e058a1ba98dd7
poohjh0515 <poohjh0515@gmail.com>
2016-06-01 14:02:45 -0500

    sayhi.sh removed. sub_phytoMorph.sh will replace it

--- main_phytoMorph.sh
+++ main_phytoMorph.sh
@@ -3,6 +3,6 @@ echo "Download started"
 wget https://bitbucket.org/leeatuw/repository_experiment/raw/master/sayhi.sh
 echo "Download ended"
 echo "shell started"
-./sayhi.sh
+./sub_phytoMorph.sh
 echo "shell ended"
 
