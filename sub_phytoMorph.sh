d48055f70c21ff506b0a19e5ad4e0a56fa3f84f4
poohjh0515 <poohjh0515@gmail.com>
2016-06-01 15:01:44 -0500

    sudo removed

--- sub_phytoMorph.sh
+++ sub_phytoMorph.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ################################################################################################################
+echo "Create directory for MCR-v717.zip"
 mkdir -p /opt/imagePhenomics/common/
 echo "Start download: MCR-v717.zip"
 wget -O /opt/imagePhenomics/common/v717.zip http://davos.cyverse.org/irods-rest/rest/fileContents/iplant/home/nmiller/publicData/v717.zip?ticket=fCMgrszgk7H3Dy2
@@ -15,7 +16,7 @@ echo "Create directory for phytoG"
 mkdir -p /opt/imagePhenomics/phytoG/
 echo "Start download: phytoG"
 wget -O /opt/imagePhenomics/phytoG/phytoG https://bitbucket.org/leeatuw/repository_experiment/raw/master/phytoG
-sudo chmod +x /opt/imagePhenomics/phytoG/phytoG
+#sudo chmod +x /opt/imagePhenomics/phytoG/phytoG
 echo "Done download: phytoG"
 ################################################################################################################
 ################################################################################################################
@@ -26,24 +27,24 @@ echo "Done download: launch.sh"
 ################################################################################################################
 echo "Start download: run_phytoG.sh "
 wget -O /opt/imagePhenomics/phytoG/run_phytoG.sh https://bitbucket.org/leeatuw/repository_experiment/raw/master/run_phytoG.sh
-sudo chmod +x /opt/imagePhenomics/phytoG/run_phytoG.sh  
+#sudo chmod +x /opt/imagePhenomics/phytoG/run_phytoG.sh  
 echo "Done download: run_phytoG.sh "
 ################################################################################################################
 ################################################################################################################
-echo "start unzip: vv717.zip"
+echo "start unzip: v717.zip"
 mkdir -p /opt/imagePhenomics/common/MATLAB/MATLAB_Compiler_Runtime_R2012a/
 #unzip /opt/imagePhenomics/common/v717.zip -C /opt/imagePhenomics/common/MATLAB/MATLAB_Compiler_Runtime_R2012a/
-sudo unzip /opt/imagePhenomics/common/v717.zip -d /opt/imagePhenomics/common/MATLAB/MATLAB_Compiler_Runtime_R2012a/
-echo "Done unzip: vv717.zip"
+unzip /opt/imagePhenomics/common/v717.zip -d /opt/imagePhenomics/common/MATLAB/MATLAB_Compiler_Runtime_R2012a/
+echo "Done unzip: v717.zip"
 ################################################################################################################
 ################################################################################################################
-echo "Start export: vv717.zip"
+echo "Start export: v717.zip"
 export MCR_CACHE_ROOT=$PWD/mcr_cache
-echo "Done export: vv717.zip"
+echo "Done export: v717.zip"
 ################################################################################################################
 ################################################################################################################
 echo "Start run: shell for compile"
 mkdir -p $MCR_CACHE_ROOT
-sudo chmod +x launch.sh
-sudo ./launch.sh
+#sudo chmod +x launch.sh
+./launch.sh
 echo "Done run: shell for compile"
