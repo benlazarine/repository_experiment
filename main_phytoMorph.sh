98b5af1e8e4fd1497acc5b0aed4047220c185911
poohjh0515 <poohjh0515@gmail.com>
2016-06-01 15:18:23 -0500

    chmod added

--- main_phytoMorph.sh
+++ main_phytoMorph.sh
@@ -3,6 +3,7 @@ echo "Download started"
 wget -O sub_phytoMorph.sh https://bitbucket.org/leeatuw/repository_experiment/raw/master/sub_phytoMorph.sh
 echo "Download ended"
 echo "shell started"
+sudo chmod +x sub_phytoMorph.sh
 sudo ./sub_phytoMorph.sh
 echo "shell ended"
 
--- main_phytoMorph.sh~
+++ main_phytoMorph.sh~
@@ -3,6 +3,6 @@ echo "Download started"
 wget -O sub_phytoMorph.sh https://bitbucket.org/leeatuw/repository_experiment/raw/master/sub_phytoMorph.sh
 echo "Download ended"
 echo "shell started"
-./sub_phytoMorph.sh
+sudo ./sub_phytoMorph.sh
 echo "shell ended"
 
