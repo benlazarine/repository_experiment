521e090728a163c73c063f7f21a373c1d2836a7e
poohjh0515 <poohjh0515@gmail.com>
2016-06-01 14:59:04 -0500

    launch.sh runs without sudo

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo /opt/imagePhenomics/phytoG/run_phytoG.sh /opt/imagePhenomics/common/MATLAB/MATLAB_Compiler_Runtime_R2012a/MATLAB_Compiler_Runtime/v717
+/opt/imagePhenomics/phytoG/run_phytoG.sh /opt/imagePhenomics/common/MATLAB/MATLAB_Compiler_Runtime_R2012a/MATLAB_Compiler_Runtime/v717
