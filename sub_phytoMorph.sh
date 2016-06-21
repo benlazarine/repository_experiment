ad91be1b58d031241b21fb86c5c9d311ecdb768c
poohjh0515 <poohjh0515@gmail.com>
2016-06-21 10:44:52 -0500

    Copied working files to master

--- sub_phytoMorph.sh
+++ sub_phytoMorph.sh
@@ -1,42 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
-			# $1 progName="phytoG";
-			# $2 REPO="https://bitbucket.org/leeatuw/repository_experiment/raw/master"
-			# echo $progName
-			#$3 MCRver="v717";
-			#echo $MCRver
-			#$4 MRCticN="HlB1NJlWX8f8mqJ";
-			#$5 mainP="/opt/imagePhenomics";
-			#$6 MCRdownP="$mainP/common"
-			#$7 MCRruntimeVer="R2012a"
-			#$8 MCRuzipP="$MCRdownP/MATLAB/MATLAB_Compiler_Runtime_$MCRruntimeVer";
-################################################################################################################
-# download environment configuration script
-echo "Start download: envConfig_phytoMorph.sh"
-wget -O download_phytoMorph.sh $2/download_phytoMorph.sh
-echo "Done download: envConfig_phytoMorph.sh"
-sudo chmod +x download_phytoMorph.sh
-./download_phytoMorph.sh $2 $1 $5
-################################################################################################################
-################################################################################################################
-# $1: MCR version: MCRver
-# $2: ticket number: MRCticN
-# $3: MCR download path: MCRdownP
-# $4: MCR uzip path: MCRuzipP
-# $5: MCR runtime version: MCRruntimeVer
-# run environment configuration script
-echo "Start run: envConfig_phytoMorph.sh"
-sudo chmod +x envConfig_phytoMorph.sh
-./envConfig_phytoMorph.sh $3 $4 $6 $8 $7
-echo "End run: envConfig_phytoMorph.sh"
-################################################################################################################
-################################################################################################################
-# $1: program name
-# $2: program path
-# $3: MCR version
-# $4: MCR runtime version
-# $5: MCR path
-=======
 ################################################################################################################
 echo "Create directory for MCR-v717.zip"
 mkdir -p /opt/imagePhenomics/common/
@@ -72,7 +34,7 @@ echo "Done download: run_phytoG.sh "
 echo "start unzip: v717.zip"
 mkdir -p /opt/imagePhenomics/common/MATLAB/MATLAB_Compiler_Runtime_R2012a/
 #unzip /opt/imagePhenomics/common/v717.zip -C /opt/imagePhenomics/common/MATLAB/MATLAB_Compiler_Runtime_R2012a/
-sudo unzip -q /opt/imagePhenomics/common/v717.zip -d /opt/imagePhenomics/common/MATLAB/MATLAB_Compiler_Runtime_R2012a/
+sudo unzip -o -q /opt/imagePhenomics/common/v717.zip -d /opt/imagePhenomics/common/MATLAB/MATLAB_Compiler_Runtime_R2012a/
 echo "Done unzip: v717.zip"
 ################################################################################################################
 ################################################################################################################
@@ -81,8 +43,8 @@ export MCR_CACHE_ROOT=$PWD/mcr_cache
 echo "Done export: v717.zip"
 ################################################################################################################
 ################################################################################################################
->>>>>>> parent of 41a2eb3... unzip overwrite added
 echo "Start run: shell for compile"
+mkdir -p $MCR_CACHE_ROOT
 sudo chmod +x launch.sh
-./launch.sh $1 $5 $3 $7 $6 
+./launch.sh
 echo "Done run: shell for compile"
