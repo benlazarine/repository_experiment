085819a42db26b0be34b1f8b0047eea2a8472387
poohjh0515 <poohjh0515@gmail.com>
2016-06-22 12:42:11 -0500

    directory mange

--- main_phytoMorph.sh
+++ main_phytoMorph.sh
@@ -27,15 +27,14 @@ select selection in "phytoG" "yart" "EXIT"; do
 		EXIT ) echo 'Now exiting'; exit;;
 	esac
 done
-
-mkdir -p "/home/phytoMorph"
-sudo cd "/home/phytoMorph"
+instPath="/home/phytoMorph"
+sudo mkdir -p $instPath/
 echo "Start download"
 wget -O sub_phytoMorph.sh $REPO/sub_phytoMorph.sh
 echo "End download"
 echo "Start run "
 sudo chmod +x sub_phytoMorph.sh
-sudo ./sub_phytoMorph.sh $progName $REPO $MCRver $MRCticN $mainP $MCRdownP $MCRruntimeVer $MCRuzipP
+sudo ./sub_phytoMorph.sh $progName $REPO $MCRver $MRCticN $mainP $MCRdownP $MCRruntimeVer $MCRuzipP $instPath
 echo "End run "
 
 
