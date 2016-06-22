6768b8178e532bdcb59710c5283aadda832060f6
poohjh0515 <poohjh0515@gmail.com>
2016-06-22 10:54:13 -0500

    phytoG and YART iconization initiated

---
+++ phytoG_GUI.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+progName="phytoG"; 
+REPO="https://bitbucket.org/leeatuw/repository_experiment/raw/Jun202016";
+MCRver="v717";
+MRCticN="HlB1NJlWX8f8mqJ";
+mainP="/opt/imagePhenomics";
+MCRdownP=$mainP"/common"
+MCRruntimeVer="R2012a"
+MCRuzipP="$MCRdownP/MATLAB/MATLAB_Compiler_Runtime_$MCRruntimeVer";
+
+echo "Start download"
+wget -O sub_phytoMorph.sh $REPO/sub_phytoMorph.sh
+echo "End download"
+echo "Start run "
+sudo chmod +x sub_phytoMorph.sh
+sudo ./sub_phytoMorph.sh $progName $REPO $MCRver $MRCticN $mainP $MCRdownP $MCRruntimeVer $MCRuzipP
+echo "End run "
+
+
+
